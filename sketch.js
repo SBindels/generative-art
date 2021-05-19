d0c2655eec1fc16c8452c62463dff3e069c8c1e0
SBindels <60451829+SBindels@users.noreply.github.com>
2021-05-19 19:47:12 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,24 @@
 let phase = 0;
+let mic = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  slider = createSlider(0,10,0)
+  mic = new p5.AudioIn();
+  mic.start();
+  //slider = createSlider(0,10,0)
 }
 
 function draw() {
   background(30);
 
-  //rotateX(90);
-  //rotateZ(frameCount);
+  rotateY(frameCount / 2);
+  rotateZ(frameCount /4);
   noFill();
   stroke(255);
+  
+  var vol = mic.getLevel();
+  console.log(vol);
 
   for (var i = 0; i < 20; i++) {
     var r = map(sin(frameCount / 2), -1, 1, 0, 255);
@@ -25,11 +31,11 @@ function draw() {
 
     beginShape();
       
-      let noiseMax = slider.value();
-      for (var j = 0; j < 360; j += 10) {
+      //let noiseMax = slider.value();
+      for (var j = 0; j < 360; j +=10) {
         
-        let xoff = map(cos(j+phase),-1,1,0,noiseMax);
-        let yoff = map(sin(j),-1,1,0,noiseMax);
+        let xoff = map(cos(j+phase),-1,1,0,10);
+        let yoff = map(sin(j),-1,1,0,10);
         // var rad = i * 10;
         var rad = map(noise(xoff,yoff), 0, 1, 50, 200);
         var x = rad * cos(j);
@@ -42,7 +48,7 @@ function draw() {
     endShape(CLOSE);
       beginShape();
       for (var j = 0; j < 360; j += 10) {
-        var rad = i * 10;
+        var rad = i * 5;
         var x = rad * cos(j);
         var y = rad * sin(j);
         var z = sin(frameCount * 4 + i * 10) * 50;
